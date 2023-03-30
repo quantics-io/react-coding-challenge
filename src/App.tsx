917f294990a7ed0121a382e51d56572b181fa1ed
mdunkel <mrks.dunkel@gmail.com>
2023-03-30 11:43:21 +0200

    fetching json implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './App.module.scss';
 
 function App() {
+
+  const [forecasts, setForecasts] = useState([]);
+  useEffect(() => {
+    fetch("forecastruns.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setForecasts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+
+
   return (<div>Implement me</div>);
 }
 
