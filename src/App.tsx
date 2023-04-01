ddf47de72435329fc63f7caf3fb4f7d6e033c920
mdunkel <mrks.dunkel@gmail.com>
2023-04-01 15:55:27 +0200

    Dropdown connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { stringify } from 'querystring';
 import { useEffect, useState } from 'react';
-import ProvideForecasts from './ProvideForecasts';
 import Layout from './Layout';
 import SelectUnit from './SelectBox';
 import { ForecastRunDto } from './models';
@@ -12,13 +11,15 @@ import { Dropdown } from './Dropdown';
 
 function App() {
   const [forecasts, setForecasts] = useState<ForecastRunDto[] | null>(null);
+  const [sourceForecast, setSourceForecast] = useState<ForecastRunDto | undefined>(undefined);
+  const [targetForecast, setTargetForecast] = useState<ForecastRunDto | undefined>(undefined);
 
   useEffect(() => {
     const fetchForecasts = async () => {
       const res: Response = await fetch("forecastruns.json");
       const forecastData: any = await res.json();
 
-      const typeSafeForecastData = filterForCorrectType(forecastData);
+      const typeSafeForecastData: ForecastRunDto[] = filterForCorrectType(forecastData);
 
       setForecasts(
         typeSafeForecastData
@@ -32,17 +33,15 @@ function App() {
     if (forecasts.length === 0)
       return <div>We couldn't find any forecasts.</div>
     return (
-      // <ProvideForecasts>
       <Layout>
         <>
           <div>Forecasts are loaded</div>
           <SelectBox title={'Source'}>
-            {/* <Dropdown values={forecasts} /> */}
-            <div>Forecasts are loaded</div>
+            <Dropdown values={forecasts} selected={sourceForecast}
+              onChange={(val: ForecastRunDto) => setSourceForecast(val)} />
           </SelectBox>
         </>
       </Layout>
-      // </ProvideForecasts>
     );
   }
   return <div>Forecasts are loadig.</div>
--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,7 +1,7 @@
 import React, { FC, memo, useCallback } from 'react';
-import './App.css';
+// import './App.css';
 import { ForecastRunDto } from "./models";
-import { MenuItem, Select } from "@mui/material";
+import { FormControl, MenuItem, Select } from "@mui/material";
 import s from './Dropdown.module.scss'
 
 type DropdownProps = {
@@ -19,7 +19,11 @@ export const Dropdown: FC<DropdownProps> = memo(function Dropdown({ values, sele
     next && id !== selected?.id && onChange(next);
   }, [values, selected?.id, onChange]);
 
-  return (<Select className={s.select} value={selected?.id} onChange={(it) => handleChange(it.target.value)}>
-    {values.map(it => <MenuItem key={it.id} value={it.id}>{it.name}</MenuItem>)}
-  </Select>);
+  return (
+    <FormControl sx={{ m: 1, width: 200, bgcolor: 'white' }} variant="standard">
+      <Select className={s.select} value={selected?.id} onChange={(it) => handleChange(it.target.value)}>
+        {values.map(it => <MenuItem key={it.id} value={it.id}>{it.name}</MenuItem>)}
+      </Select>
+    </FormControl>
+  );
 });
